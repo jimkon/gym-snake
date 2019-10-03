0c37b2565854dae35d0ba0c628ce75401eea861d
jimkon <dkontzedakis@gmail.com>
2019-10-03 19:29:58 +0300

    completed game logic

--- gym_snake/envs/snake_env.py
+++ gym_snake/envs/snake_env.py
@@ -14,58 +14,72 @@ class SnakeEnv(gym.Env):
             }
 
     def __init__(self):
-        self.grid = np.zeros((15, 15), np.int)
-        self.topleft, self.bottomright = np.array([0, 0]), np.array([15, 15])
-        self.head_position = np.array([5, 10])
+        self.grid_shape = (15, 15)
+        self.grid = None
+        self.max_steps = 1000
+        self.topleft, self.bottomright = np.array([0, 0]), np.array(list(self.grid_shape))
+
+        self.head_position = None
+
         self.initial_length = 3
-        self.directions = np.array([[-1, 0], [0, -1], [1, 0], [0, 1]], np.int)
+        self.directions = np.array([[-1, 0], [0, -1], [1, 0], [0, 1]])
         self.direction_pointer = 0
-        self.action = 0
-        self.snake_heading = self.directions[self.direction_pointer]
-        self.snake_body = collections.deque([self.head_position])
-        for i in range(self.initial_length-1):
-            self.snake_body.append(self.head_position+i*self.snake_heading)
 
-        self.direction_pointer = 2
-
-        for i in range(len(self.snake_body)):
-            y, x = yx = self.snake_body.popleft()
-            self.grid[y][x] = 1
-            self.snake_body.append(yx)
-
-        # render
-        pygame.init()
-        self.size = self.width, self.height = 600, 600
-        self.rect_width, self.rect_height = tuple(self.size/np.array(list(self.grid.shape)))
-        self.screen = None
+        self.snake_heading = None
+        self.snake_body = None
 
     def step(self, action):
-        self.action = np.clip(action, -1, 1)
-        self.direction_pointer = (self.direction_pointer+self.action)%len(self.directions)
+        action = 1 if action>1 else -1 if action<-1 else action
+        # action = max(min(action, 1), -1)
+        self.direction_pointer = (self.direction_pointer+action)%len(self.directions)
         self.snake_heading = self.directions[self.direction_pointer]
         head_y, head_x = new_head_yx = self.head_position + self.snake_heading
-        if np.any(new_head_yx<self.topleft)\
-                or np.any(new_head_yx>=self.bottomright)\
-                or self.grid[head_y][head_x] == 1:
+
+        if head_y<self.topleft[0] or head_y>=self.bottomright[0]\
+                or head_x<self.topleft[1] or head_x>=self.bottomright[1]\
+                or self.grid[head_y][head_x] == 1.\
+                or self.current_step>=self.max_steps:
             return self.grid, -1, True, {}
 
-        if self.grid[head_y][head_x] == 2:
+        if self.grid[head_y][head_x] == 2.:
             reward = 1
+            self.grid[head_y][head_x] = 1.
+            self.generate_new_food()
         else:
             tail_y, tail_x = self.snake_body.pop()
-            self.grid[tail_y][tail_x] = 0
+            self.grid[tail_y][tail_x] = 0.
             reward = 0
 
-        self.grid[head_y][head_x] = 1
+        self.grid[head_y][head_x] = 1.
         self.head_position = new_head_yx
         self.snake_body.appendleft(new_head_yx)
 
+        self.current_step += 1
         return self.grid, reward, False, {}
 
     def reset(self):
-        pass
+        self.grid = np.zeros(self.grid_shape)
+        self.current_step = 0
+        self.head_position = np.array([5, 10])
+        self.direction_pointer = 0
+
+        self.snake_heading = self.directions[self.direction_pointer]
+        self.snake_body = collections.deque([self.head_position])
+        self.grid[self.head_position[0]][self.head_position[1]] = 1
+        for i in range(1, self.initial_length):
+            y, x = yx = self.head_position + i * self.snake_heading
+            self.snake_body.append(yx)
+            self.grid[y][x] = 1.
+
+        self.direction_pointer = 2
+        self.generate_new_food()
+        return self.grid
 
     def render(self, mode='human', close=False):
+        pygame.init()
+        self.size = self.width, self.height = 600, 600
+        self.rect_width, self.rect_height = tuple(self.size / np.array(list(self.grid_shape)))
+        self.screen = None
         if self.screen is None:
             self.screen = pygame.display.set_mode(self.size)
 
@@ -89,5 +103,13 @@ class SnakeEnv(gym.Env):
                          pygame.Rect(x * self.rect_width, y * self.rect_height, self.rect_width, self.rect_height))
 
         pygame.display.flip()
-        time.sleep(.33)
+        # time.sleep(.333)
+
+    def generate_new_food(self):
+        probs = 1-self.grid.flatten()
+        probs /= probs.sum()
+        ind = np.random.choice(len(probs), 1, p=probs)[0]
+        y, x = ind//self.grid_shape[0], ind%self.grid_shape[0]
+        self.grid[y][x] = 2.
+
 
