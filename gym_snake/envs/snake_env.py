0f0ccd6b10bd8b71d18bd02b1ffad6aa9f3b1a3f
jimkon <dkontzedakis@gmail.com>
2019-10-04 17:46:48 +0300

    2d

--- gym_snake/envs/snake_env.py
+++ gym_snake/envs/snake_env.py
@@ -7,7 +7,7 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-
+# 2D grid implementation
 class SnakeEnv(gym.Env):
     metadata = {
             'render.modes': ['human']
