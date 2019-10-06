4749a19ce5e12d4eb6a6315ad13481c5354987ba
jimkon <dkontzedakis@gmail.com>
2019-10-06 16:39:57 +0300

    package name

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from setuptools import setup
 
-setup(name='gym_snake', version = '0.1', install_requires = ['gym', 'numpy', 'pygame'])
+setup(name='gym_snake',
+      version='0.2',
+      install_requires=['gym', 'numpy', 'pygame'])
